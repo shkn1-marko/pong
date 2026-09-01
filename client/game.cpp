4e518b96933b4db518087c8dda5fe4c7f85e9970
shkn1-marko <shkn1.marko@gmail.com>
2026-09-01 16:01:52 +0200

    feat: implement PongGame processInput, update and render methods

--- client/game.cpp
+++ client/game.cpp
@@ -37,3 +37,58 @@ void PongGame::resetBall()
                          windowHeight / 2.0f - ball.size.y / 2.0f);
     ball.velocity = glm::vec2(200.0f, 150.0f);
 }
+
+void PongGame::processInput(GLFWwindow* window, float dt)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        leftPaddle.moveUp(dt, 0.0f);
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        leftPaddle.moveDown(dt, (float)windowHeight);
+
+    if (glfwGetKey(window, GLFW_KEY_K) == GLFW_PRESS)
+        rightPaddle.moveUp(dt, 0.0f);
+    if (glfwGetKey(window, GLFW_KEY_J) == GLFW_PRESS)
+        rightPaddle.moveDown(dt, (float)windowHeight);
+}
+
+void PongGame::update(float dt)
+{
+    if (gameOver) return;
+
+    ball.update(dt, 0.0f, (float)windowHeight);
+
+    if (checkCollision(ball.pos, ball.size, leftPaddle.pos, leftPaddle.size))
+        ball.velocity.x = -ball.velocity.x;
+    if (checkCollision(ball.pos, ball.size, rightPaddle.pos, rightPaddle.size))
+        ball.velocity.x = -ball.velocity.x;
+
+    if (ball.pos.x < 0.0f)
+    {
+        rightScore++;
+        if (rightScore >= winningScore) gameOver = true;
+        else resetBall();
+    }
+    if (ball.pos.x + ball.size.x > (float)windowHeight)
+    {
+        leftScore++;
+        if (leftScore >= winningScore) gameOver = true;
+        else resetBall();
+    }
+}
+
+void PongGame::render(QuadRenderer& renderer)
+{
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    drawNet(renderer);
+
+    renderer.draw(leftPaddle.pos, leftPaddle.size, glm::vec3(1.0f, 1.0f, 1.0f));
+    renderer.draw(rightPaddle.pos, rightPaddle.size, glm::vec3(1.0f, 1.0f, 1.0f));
+    renderer.draw(ball.pos, ball.size, glm::vec3(1.0f, 1.0f, 1.0f));
+
+    drawScore(renderer);
+}
