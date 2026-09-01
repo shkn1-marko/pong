8a89cdb90781e2714b6b90ebb4012fa3b3ef6146
shkn1-marko <shkn1.marko@gmail.com>
2026-09-01 12:25:44 +0200

    refactor: add PongGame class skeleton to encapsulate game state

---
+++ client/game.hpp
@@ -0,0 +1,42 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+
+#include "paddle.hpp"
+#include "ball.hpp"
+#include "renderer.hpp"
+
+class PongGame
+{
+public:
+    PongGame(int windowWidth, int windowHeight);
+
+    void processInput(GLFWwindow* window, float dt);
+    void update(float dt);
+    void render(QuadRenderer& renderer);
+
+    bool isGameOver() const { return gameOver; }
+
+private:
+    int windowWidth;
+    int windowHeight;
+    int winningScore = 3;
+
+    Paddle leftPaddle;
+    Paddle rightPaddle;
+    Ball ball;
+
+    int leftScore = 0;
+    int rightScore = 0;
+    bool gameOver = false;
+
+    bool checkCollision(const glm::vec2& posA, const glm::vec2& sizeA,
+                        const glm::vec2& posB, const glm::vec2& sizeB);
+
+    void resetBall();
+    void drawDigit(QuadRenderer& renderer, int digit, glm::vec2 pos, float scale, glm::vec3 color);
+    void drawScore(QuadRenderer& renderer);
+    void drawNet(QuadRenderer& renderer);
+};
