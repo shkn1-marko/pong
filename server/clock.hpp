591c8b6409a3b535611584949bc741718f45b80a
shkn1-marko <shkn1.marko@gmail.com>
2026-09-04 21:39:52 +0200

    feat: add TickManager skeleton and shared clock.hpp type aliases

---
+++ server/clock.hpp
@@ -0,0 +1,6 @@
+#pragma once
+
+#include <chrono>
+
+using Clock = std::chrono::steady_clock;
+using TimePoint = Clock::time_point;
---
+++ server/tick_manager.hpp
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <array>
+#include <chrono>
+#include <cstdint>
+#include <optional>
+
+#include "player_input_queue.hpp"
+#include "protocol.hpp"
+#include "clock.hpp"
+
+const int NUM_PLAYERS = 2;
+
+class TickManager
+{
+public:
+    void submitInput(int playerId, const InputPacket& packet);
+
+    std::optional<StatePacket> tryAdvance(TimePoint now);
+
+private:
+    std::array<PlayerInputQueue, NUM_PLAYERS> queues;
+
+    uint32_t currentTick = 0;
+    TimePoint tickStartTime = Clock::now();
+
+    bool allReady();
+    StatePacket buildStatePacket();
+};
