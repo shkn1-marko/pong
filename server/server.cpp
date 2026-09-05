af5c032fa180835ba35f9b30be0e46593146749e
shkn1-marko <shkn1.marko@gmail.com>
2026-09-05 08:06:26 +0200

    feat: implement identifyPlayer and broadcast for PongServer

--- server/server.cpp
+++ server/server.cpp
@@ -42,3 +42,40 @@ bool PongServer::setupSocket()
     std::cout << "Server listening on port " << port << "\n";
     return true;
 }
+
+std::optional<int> PongServer::identifyPlayer(const sockaddr_in& senderAddr)
+{
+    for (int i = 0; i < NUM_PLAYERS; i++)
+    {
+        if (playerAddrs[i].has_value() &&
+            playerAddrs[i]->sin_addr.s_addr == senderAddr.sin_addr.s_addr &&
+            playerAddrs[i]->sin_port == senderAddr.sin_port)
+        {
+            return i;
+        }
+    }
+
+    for (int i = 0; i < NUM_PLAYERS; i++)
+    {
+        if (!playerAddrs[i].has_value())
+        {
+            playerAddrs[i] = senderAddr;
+            std::cout << "Player " << i << " connected\n";
+            return i;
+        }
+    }
+
+    return std::nullopt;
+}
+
+void PongServer::broadcast(const StatePacket& packet)
+{
+    for (int i = 0; i < NUM_PLAYERS; i++)
+    {
+        if (playerAddrs[i].has_value())
+        {
+            sendto(sockfd, &packet, sizeof(packet), 0,
+                   (sockaddr*)&playerAddrs[i].value(), sizeof(sockaddr_in));
+        }
+    }
+}
