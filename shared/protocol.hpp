a7a1b403a32ad76f9b3e00a7cb6e8def3d7332a0
shkn1-marko <shkn1.marko@gmail.com>
2026-09-06 21:38:46 +0200

    refactor: add PacketType tag to all protocol messages, add JoinRequest/JoinAccept

--- shared/protocol.hpp
+++ shared/protocol.hpp
@@ -2,8 +2,28 @@
 
 #include <cstdint>
 
+enum class PacketType : uint32_t
+{
+    Join = 0,
+    JoinAccept = 1,
+    Input = 2,
+    State = 3
+};
+
+struct JoinRequest
+{
+    PacketType type = PacketType::Join;
+};
+
+struct JoinAccept
+{
+    PacketType type = PacketType::JoinAccept;
+    int32_t playerId;
+};
+
 struct InputPacket
 {
+    PacketType type = PacketType::Input;
     uint32_t tick;
     bool up;
     bool down;
@@ -11,6 +31,7 @@ struct InputPacket
 
 struct StatePacket
 {
+    PacketType type = PacketType::State;
     uint32_t tick;
     bool player1Up;
     bool player1Down;
